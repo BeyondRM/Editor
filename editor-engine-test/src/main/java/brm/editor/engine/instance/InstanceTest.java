740c603789a0c285cfbaf45f4ffdcd048bba854b
JustGregory <gregory.cheyney@gmail.com>
2016-01-14 17:59:18 -0600

    Add 'editor-engine-test' module.
    Initial files for the testing of game projects from the editor. Does
    nothing just yet; waiting other features.

---
+++ editor-engine-test/src/main/java/brm/editor/engine/instance/InstanceTest.java
@@ -0,0 +1,11 @@
+package brm.editor.engine.instance;
+
+
+/**
+ * An instance test definition.
+ * @author Gregory
+ */
+abstract public class InstanceTest {
+  public InstanceTest() {
+  }
+}
---
+++ editor-engine-test/src/main/java/brm/editor/engine/instance/TestBattles.java
@@ -0,0 +1,21 @@
+package brm.editor.engine.instance;
+
+
+/**
+ * An instance of a battle test.
+ * @author Gregory
+ */
+public class TestBattles extends InstanceTest {
+  /**
+   * A public constructor.
+   * <p/>
+   * This is one of two ways to allow test-playing of battle scenarios; the other way is via including the test classes
+   * in the game engine itself &mdash; but then it would be available for players as well. Not necessarily a bad thing,
+   * but some game authors may not want their target players to be able to select random battles to play, over and over
+   * again, because then the players might start pointing out monster and character imbalances that a game author might
+   * know about already. &mdash; On the other hand, including a free battle-test in the engine release might be desired
+   * by many players for an occasional diversion from the main game.
+   */
+  public TestBattles() {
+  }
+}
